da2c115997aacafda699025481c03dbde6bab74f
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2023-11-26 04:34:16 +0100

    Delete Spectron directory

--- Spectron/Package/Spectron 2.5/js/app.cjs
+++
@@ -1,8 +0,0 @@
-// Test Connection JavaScript Compatibility [Remove if you no longer need]...
-
-/* Try running these
-    alert($('layout').parentElement.nodeName);
-    alert(`Welcome To ${app.name}. Created by ${app.author}. Copyright ${app.copyright}.`);
-    alert(d.fullDate());
-*/
-import_Stylesheets(["css/root.css"]);
