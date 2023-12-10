149bc2c3dcf094f165c7d587086f617efb00f7c8
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2023-12-10 01:47:09 +0100

    Delete SpectronX/Package/SpectronX directory

--- SpectronX/Package/SpectronX/js/app.cjs
+++
@@ -1,8 +0,0 @@
-// Test Connection JavaScript Compatibility [Remove if you no longer need]...
-
-/* Try running these
-    alert($('layout').parentElement.nodeName);
-    alert(`Welcome To ${app.name}. Created by ${app.author}. Copyright ${app.copyright}.`);
-    alert(d.fullDate());
-*/
-importStylesheets(["css/root.css"]);
