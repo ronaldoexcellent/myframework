6145cc7fe19d1d1345fd586ee925b395e4db06c9
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2023-12-11 01:10:38 +0100

    Add files via upload

---
+++ SpectronX/Package/SpectronX/package.cjs
@@ -0,0 +1,22 @@
+// Package/App Data
+// App root
+const app = {
+    name : "",
+
+    author : "",
+
+    logo : document.querySelector('application').getAttribute('logo'),
+
+    theme : "",
+
+    mode : "",
+
+    projectStart : "",
+
+    projectEnd : ""
+};
+
+// App Components
+const elements = [];
+
+const components = {};
